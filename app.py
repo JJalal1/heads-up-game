efb0c34126b8bebf2bfe9331f52d4c1919143121
JJalal1 <jlalalhmaty7@gmail.com>
2025-12-14 04:32:33 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SESSION_SECRET")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
